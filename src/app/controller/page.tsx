05b8b82c72c2179aa89f872e24024b8a39f036c4
marvinalnashi <marvinalnashi@hotmail.com>
2025-05-29 00:27:20 +0200

    Update page.tsx

--- src/app/controller/page.tsx
+++ src/app/controller/page.tsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Cookies from 'js-cookie';
 import Ably from 'ably';
-import {logSessionData} from "@/utils/sessionLogger";
 import {Timestamp} from "firebase/firestore";
 
 const ably = new Ably.Realtime({ key: process.env.NEXT_PUBLIC_ABLY_API_KEY!, clientId: 'controller-client' });
@@ -48,20 +47,6 @@ export default function ControllerPage() {
     };
 
     const exitSession = () => {
-        if (sessionId) {
-            logSessionData({
-                sessionId,
-                role: 'controller',
-                userAgent: navigator.userAgent,
-                adaptiveVolume: adaptivity === 'yes',
-                backgroundNoiseLevel: backgroundNoise,
-                events: [{
-                    event: 'exit',
-                    timestamp: Timestamp.now(),
-                }]
-            });
-        }
-
         channel.publish('reset', {});
         Cookies.remove('ably-session');
         router.push('/');
